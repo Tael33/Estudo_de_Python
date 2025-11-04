eb564bb30f7fd9e90aa2e3b013483f9c82c69502
Natanael Cardoso Brito <ncardoso392@gmail.com>
2025-11-04 07:56:55 -0300

    .

--- curso_em_video/Tratar_cadeia_de_texto/ex005.py
+++ curso_em_video/Tratar_cadeia_de_texto/ex005.py
@@ -2,3 +2,8 @@
 # Quantas vezes aparece a letra "A".
 # Em que posição ela aparece a primeira vez.
 # Em que posição ela aparece a última vez.
+
+frase = input('Digite uma frase: ').strip().upper()
+print(f'A frase tem {frase.count('A')} letras "A"')
+print(f'A primeira letra "A" aparece na posição {frase.find('A') + 1}')
+print(f'A última vez que a letra "A" aparece está na posição {frase.rfind("A") + 1}')
